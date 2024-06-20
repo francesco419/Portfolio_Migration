9ad5b384485214706066114ad396e45699852c3e
Lee Sang Hyeon <francesco419@naver.com>
2024-06-21 00:03:58 +0900

    240621

--- src/pages/202305/hidden.jsx
+++ src/pages/202305/hidden.jsx
@@ -10,7 +10,10 @@ const TopIndicator = lazy(() => import('./top/toTop'));
 const Front = lazy(() => import('./front/front'));
 const SKillContainer = lazy(() => import('../skill/skillContainer'));
 
-const intro = `끊임없이 변화되는 코드와 함께 성장해나가려고 노력하는 신입 개발자 이상현입니다.\n무언가를 제작하는 것을 좋아했기에 코드를 사용해 생각하는 것을 만들어 낼 수 있는\n개발에 큰 흥미를 느껴 개발자가 되고자 결심하게 되었으며, 사용자 경험을 중시하는 마인드를 가지고\n보다 정확하고 편리한 서비스를 제공하는 개발자가 되고자 합니다.`;
+const INTRO = `끊임없이 변화되는 코드와 함께 성장해나가려고 노력하는 신입 개발자 이상현입니다.
+복잡한 문제를 쉽게 해결하는 클린코드 작성을 통해 프로덕트의 퍼포먼스를 향상시키고,
+높은 사용자 경험을 제공하기 위해 차별화된 아이디어를 모색하며 다양한 UI/UX를 구현하고자 합니다.
+이와 같은 노력과 목표를 바탕으로 누구나 머무르고 싶은 WEB을 만드는 개발자 되고자 합니다.`;
 
 export default function Hidden() {
   const about = useRef(),
@@ -44,7 +47,7 @@ export default function Hidden() {
           </div>
           <Front />
           <div className={styles['hidden-intro']}>
-            <p>{intro}</p>
+            <p>{INTRO}</p>
           </div>
           <AboutContainer forwardRef={about} />
           <Suspense>
