3857dd26f9c79982b0e8b46eaaa2de15a6cc9ef8
Lee Sang Hyeon <francesco419@naver.com>
2024-07-05 20:18:14 +0900

    0705 build fix

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import { visualizer } from 'rollup-plugin-visualizer';
+import compression from 'vite-plugin-compression2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     warmup: {
+      //클라이언튼에서 자주 사용하는 파일을 미리 로드 , 우선순위 up
       clientFiles: [
         './src/style.css',
         './src/components/notice/notice.module.css'
@@ -16,10 +18,10 @@ export default defineConfig({
   plugins: [
     react(),
     svgr(),
-    visualizer({
-      filename: './dist/report.html',
-      open: true,
-      brotliSize: true
+    compression({
+      // gzip 압축, 확장자 .gz
+      algorithm: 'gzip',
+      ext: '.gz'
     })
   ],
   assetsInclude: ['**/*.PNG'],
@@ -36,9 +38,23 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
+      plugins: [
+        visualizer({
+          filename: './dist/report.html',
+          open: true,
+          brotliSize: true
+        })
+      ],
       output: {
         manualChunks: {
-          lodash: ['lodash']
+          lodash: ['lodash'],
+          react: ['react', 'react-dom'],
+          reactRouter: ['react-router-dom'],
+          redux: ['react-redux'],
+          animations: ['framer-motion'],
+          drei: ['@react-three/drei'],
+          fiber: ['@react-three/fiber'],
+          three: ['three']
         }
       }
     }
