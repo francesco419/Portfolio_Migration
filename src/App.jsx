0d0d9c6618062a48f994d42ac5984dd15a1927ef
Lee Sang Hyeon <francesco419@naver.com>
2024-07-20 22:36:25 +0900

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import { Route, Routes, HashRouter } from "react-router-dom";
-import React from "react";
-import TopIndicator from "./pages/202305/top/toTop.jsx";
-import MyHeader from "./pages/202305/myHeader.jsx";
-import FooterContact from "./pages/202305/contact/footer.jsx";
-//import LoadingPage from "./pages/loading/loadingPage.jsx";
+import { Route, Routes, HashRouter } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
+import TopIndicator from './pages/202305/top/toTop.jsx';
+import MyHeader from './pages/202305/myHeader.jsx';
+import FooterContact from './pages/202305/contact/footer.jsx';
+import LoadingPage from './pages/loading/loadingPage.jsx';
 //import Notice from "./components/notice/notice.jsx";
 
-import ProjectPage from "./pages/202305/project/projectPage/projectPage.jsx";
-import Hidden from "./pages/202305/hidden.jsx";
+//import ProjectPage from "./pages/202305/project/projectPage/projectPage.jsx";
+//import Hidden from "./pages/202305/hidden.jsx";
 
-//const ProjectPage = lazy(() => import("./pages/202305/project/projectPage/projectPage.jsx"));
-//const Hidden = lazy(() => import("./pages/202305/hidden.jsx"));
+const ProjectPage = lazy(() =>
+  import('./pages/202305/project/projectPage/projectPage.jsx')
+);
+const Hidden = lazy(() => import('./pages/202305/hidden.jsx'));
 
 function App() {
-    return (
-        <HashRouter>
-            {/* <Notice /> */}
-            <TopIndicator />
-            <MyHeader />
-            {/*             <Suspense fallback={<LoadingPage />}> */}
-            <Routes>
-                <Route path={`/project/:id`} element={<ProjectPage />} />
-                <Route path={`/`} element={<Hidden />} />
-            </Routes>
-            {/* </Suspense> */}
-            <FooterContact />
-        </HashRouter>
-    );
+  return (
+    <HashRouter>
+      {/* <Notice /> */}
+      <TopIndicator />
+      <MyHeader />
+      <Suspense fallback={<LoadingPage />}>
+        <Routes>
+          <Route path={`/project/:id`} element={<ProjectPage />} />
+          <Route path={`/`} element={<Hidden />} />
+        </Routes>
+      </Suspense>
+      <FooterContact />
+    </HashRouter>
+  );
 }
 
 export default App;
